76c05e72d2e0232bc8bafbef0ab609f7e93a9c25
Max Brown <euphrasiologist>
2022-06-10 11:33:56 +0100

    Update on: Fri 10 Jun 11:33:54 BST 2022

--- src/update.bash
+++ src/update.bash
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+DATE=$(date)
+
 bash collect_stats.bash
 
 # format data
 /software/R-4.1.3/bin/Rscript format.R > ../README.md
+
+git add ..
+git commit -m "Update on: ${DATE}"
+git push
