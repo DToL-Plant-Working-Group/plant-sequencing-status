9d3717839178716e21f7b543dcc16240e588e3c0
Max Brown <max.brown@ed.ac.uk>
2024-02-16 11:34:03 +0000

    Update on: Fri 16 Feb 11:33:59 GMT 2024

--- src/collect_stats.bash
+++ src/collect_stats.bash
@@ -7,22 +7,22 @@ printf "Fetching assemblies\n"
 
 ls /lustre/scratch124/tol/projects/darwin/data/{dicots,monocots,vascular-plants,non-vascular-plants}/*/assembly/release/reference 2>/dev/null | \
 grep "lustre" | \
-awk -F/ '{print $8,"\t",$9,"\t","released"}' > all_assembled_plant_genomes.tsv
+awk -F/ '{print $8,"\t",$9,"\t","released"}' | uniq > all_assembled_plant_genomes.tsv
 
 ls /lustre/scratch124/tol/projects/darwin/data/{dicots,monocots,vascular-plants,non-vascular-plants}/*/assembly/curated/* 2>/dev/null | \
 grep "lustre" | \
-awk -F/ '{print $8,"\t",$9,"\t","curated"}' >> all_assembled_plant_genomes.tsv
+awk -F/ '{print $8,"\t",$9,"\t","curated"}' | uniq >> all_assembled_plant_genomes.tsv
 
 ls /lustre/scratch124/tol/projects/darwin/data/{dicots,monocots,vascular-plants,non-vascular-plants}/*/assembly/draft/* 2>/dev/null | \
 grep "lustre" | \
-awk -F/ '{print $8,"\t",$9,"\t","draft"}' >> all_assembled_plant_genomes.tsv
+awk -F/ '{print $8,"\t",$9,"\t","draft"}' | uniq >> all_assembled_plant_genomes.tsv
 
 printf "Fetching genomic data\n"
 
 # Genomic data
 ls /lustre/scratch124/tol/projects/darwin/data/{dicots,monocots,vascular-plants,non-vascular-plants}/*/genomic_data/*/*/ | \
 grep "lustre" | \
-awk -F/ '{print $8,"\t",$9,"\t",$10,"\t",$11,"\t",$12}' > all_plant_samples_by_genomic_data.tsv
+awk -F/ '{print $8,"\t",$9,"\t",$10,"\t",$11,"\t",$12}' | uniq > all_plant_samples_by_genomic_data.tsv
 
 printf "Done\n"
 
